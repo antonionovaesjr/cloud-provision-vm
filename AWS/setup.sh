9137d1043c27d58431ed3bd6a94034b4e293b412
Antonio Novaes <antonio.novaes@cronapp.io>
2021-10-07 13:18:21 -0300

    erro format automaticaly

--- AWS/setup.sh
+++ AWS/setup.sh
@@ -20,14 +20,14 @@ for DEVICE_NAME in `sudo lsblk -l -o NAME|grep nvme[0-9]n[0-9]$`; do
 
 
         if [ $(grep var $HOME/lista-particao.conf|wc -l) -gt 0 ]; then
-            echo "/dev/$DEVICE_NAME$PART_DEFAULT:tmp" >> $HOME/lista-particao.conf
+            echo "/dev/$DEVICE_NAME:tmp" >> $HOME/lista-particao.conf
         else
-            echo "/dev/$DEVICE_NAME$PART_DEFAULT:var" > $HOME/lista-particao.conf
+            echo "/dev/$DEVICE_NAME:var" > $HOME/lista-particao.conf
         fi
 
     else
 
-        echo "/dev/$DEVICE_NAME$VAR_TEMP partição exisite e nenhum ação será feita"
+        echo "/dev/$DEVICE_NAME$PART_DEFAULT partição exisite e nenhum ação será feita"
 
     fi
 
@@ -36,11 +36,13 @@ done
 
 DEVICE_TO_VAR=$(grep var $HOME/lista-particao.conf|cut -d\: -f1)
 DEVICE_TO_TMP=$(grep tmp $HOME/lista-particao.conf|cut -d\: -f1)
+
 sudo parted -a optimal $DEVICE_TO_VAR mklabel msdos -- 'mkpart primary ext4 1 -1'
 sudo parted -a optimal $DEVICE_TO_TMP mklabel msdos -- 'mkpart primary ext4 1 -1'
 
 sudo mkfs.ext4 -L particao-var $DEVICE_TO_VAR$PART_DEFAULT
 sudo mkfs.ext4 -L particao-temp $DEVICE_TO_TMP$PART_DEFAULT
+
 sudo cp /etc/fstab /etc/fstab.bck
 sudo chmod ugo+rw /etc/fstab
 sudo echo "LABEL=particao-var     /var/log    ext4   defaults 0 0" >> /etc/fstab
